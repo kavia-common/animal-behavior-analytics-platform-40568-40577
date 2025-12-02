2dca10523ae35896cb85adc0a44651d7e8eb7bf4
Kavia AI <production@kavia.ai>
2025-12-02 07:48:36 +0000

    Checkpoint 20 - cg57b3a0e6

--- frontend_react_js/vite.config.js
+++ frontend_react_js/vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig(function () {
         'vscode-internal-12784-beta.beta01.cloud.kavia.ai',
         'localhost',
         '127.0.0.1',
-        'vscode-internal-25220-beta.beta01.cloud.kavia.ai'
+        'vscode-internal-25220-beta.beta01.cloud.kavia.ai',
+        'vscode-internal-40254-beta.beta01.cloud.kavia.ai',
     ];
     return {
         base: base,
